1a91fe5ec15225974962be2f3a1adc4905cea6c4
ezzye <ezzy.elliott@gmail.com>
2020-02-17 09:26:49 +0000

    WIP Add steps to component tests

--- component-test/cucumber/features/step_definitions/my_steps.rb
+++ component-test/cucumber/features/step_definitions/my_steps.rb
@@ -1,9 +1,17 @@
 When(/^the lambda is triggered with web email message that contains$/) do |table|
   # table is a table.hashes.keys # => [:post_code, :N16 8BR]
-  pending
+  body = table.rows_hash.symbolize_keys
+  @message_id = SecureRandom.uuid
+  # Make event
+  event = SqsEvent.new.put_record(body, message_id: @message_id)
+  # Trigger Lambda
+  @under_test = LambdaTrigger.new(event)
 end
 
 Then(/^a message is published to the output topic that contains$/) do |table|
   # table is a table.hashes.keys # => [:post_code, :N16 8BR]
-  pending
+  sns_message = OUTPUT_TOPIC.sns.receive
+  expect(sns_message.message_id).not_to be(nil)
+  actual = JSON.parse(sns_message['body'], symbolize_names: true)
+  expected = table.rows_hash.symbolize_keys
 end
